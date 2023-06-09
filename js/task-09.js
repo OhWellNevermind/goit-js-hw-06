58171a35de92012135a1957ce9366abe26fe99a2
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-06-09 18:01:01 +0300

    task-9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorChangeBtn = document.querySelector(".change-color");
+const background = document.querySelector("body");
+const colorHex = document.querySelector(".color");
+
+colorChangeBtn.addEventListener("click", () => {
+  let colorToChange = getRandomHexColor();
+  background.style.backgroundColor = colorToChange;
+  colorHex.textContent = colorToChange;
+  colorChangeBtn.style.backgroundColor = colorToChange;
+})
