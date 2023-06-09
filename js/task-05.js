bf86230e13bdd3dc0efd5ae0e994c8b317909007
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-06-09 11:04:58 +0300

    task-5 completed

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+const nameOutput = document.querySelector("#name-output")
+const nameInput = document.querySelector("#name-input")
+
+nameInput.addEventListener("input", (event) => {
+    nameOutput.textContent = event.currentTarget.value;
+    if (event.currentTarget.value === "") {
+        nameOutput.textContent = "Anonymous"
+    }
+})
