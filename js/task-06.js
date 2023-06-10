ed276feb2bb692dd1eb6a05ae55c49d031ebaa90
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-06-10 13:07:53 +0300

    task-6 fixed

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,11 @@ const input = document.querySelector("#validation-input");
 
 input.addEventListener("blur", (event) => {
     if (event.currentTarget.value.length === +event.currentTarget.dataset.length) {
+        event.currentTarget.classList.remove("invalid")
         event.currentTarget.classList.add("valid")
     }
     else {
+        event.currentTarget.classList.remove("valid")
         event.currentTarget.classList.add("invalid")
     }
 })
