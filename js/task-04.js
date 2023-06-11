457f61acada57664e4c082d3a4bc5d835f07738e
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-06-11 13:52:22 +0300

    task-4 fixed

--- js/task-04.js
+++ js/task-04.js
@@ -2,9 +2,9 @@ const increment = document.querySelector('[data-action="increment"]');
 const decrement = document.querySelector('[data-action="decrement"]');
 const value = document.querySelector("#value");
 increment.addEventListener("click", () => {
-    value.innerHTML = +value.innerHTML + 1
+    value.textContent = +value.textContent + 1
 })
 
 decrement.addEventListener("click", () => {
-    value.innerHTML = +value.innerHTML - 1
+    value.textContent = +value.textContent - 1
 })
