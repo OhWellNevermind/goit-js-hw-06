5c183b8ec8b06c9313f35e3f3313f26a9d9fc74d
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-06-11 13:46:04 +0300

    task-2 fixed

--- js/task-02.js
+++ js/task-02.js
@@ -8,13 +8,12 @@ const ingredients = [
 ];
 
 const list = document.querySelector("#ingredients");
-let fragList = document.createDocumentFragment();
 
-ingredients.forEach(ingredient => {
+const liList = ingredients.map(ingredient => {
   const element = document.createElement("li")
   element.textContent = ingredient;
-  element.className = "item"
-  fragList.appendChild(element);
+  element.className = 'item'
+  return element.outerHTML
 })
 
-list.append(fragList);
+list.insertAdjacentHTML("beforeend", liList.join(""));
