74f381c097dba791cda7a4b844a521ef3e4345d9
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-06-11 14:38:08 +0300

    task-10 fixed

--- js/task-10.js
+++ js/task-10.js
@@ -1,10 +1,3 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
-
 
 const createBtn = document.querySelector('[data-create]')
 const destroyBtn = document.querySelector('[data-destroy]')
@@ -17,21 +10,32 @@ createBtn.addEventListener("click", function () {
 
 destroyBtn.addEventListener("click", destroyBoxes)
 
-function createBoxes(amount) {
-  let width = "20px"
-  let heigth = "20px"
-  for (let i = 0; i < amount; i += 1) {
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
-    const div = document.createElement("div");
-    div.style.backgroundColor = getRandomHexColor();
-    width = 10 + parseInt(width) + "px"
-    heigth = 10 + parseInt(heigth) + "px"
-    div.style.width = width;
-    div.style.height = heigth;
-    boxContainer.insertAdjacentHTML("beforeend", `${div.outerHTML}`)
+function createBoxes(amount) {
+  if (amount < +boxesCounter.min || amount > +boxesCounter.max) {
+    alert("Please enter value in range from 1 to 100")
+  } else {
+    let width = "20px"
+    let heigth = "20px"
+    for (let i = 0; i < amount; i += +boxesCounter.step) {
+
+      const div = document.createElement("div");
+      div.style.backgroundColor = getRandomHexColor();
+      width = 10 + parseInt(width) + "px"
+      heigth = 10 + parseInt(heigth) + "px"
+      div.style.width = width;
+      div.style.height = heigth;
+      boxContainer.insertAdjacentHTML("beforeend", `${div.outerHTML}`)
+    }
   }
 }
 
 function destroyBoxes() {
   boxContainer.innerHTML = "";
+  boxesCounter.value = "";
 }
