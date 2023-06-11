5d05b6470910acd8e0859b5efcde32150bc5d338
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-06-11 13:17:08 +0300

    task-1 fixed with DOM nav

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,7 @@ const categoryList = document.querySelectorAll("#categories .item")
 // console.log(categoryCount);
 console.log("Number of categoris: ", categoryCount);
 categoryList.forEach(el => {
-    console.log("Category: ", el.querySelector("h2").innerHTML);
-    console.log("Elements: ", el.querySelectorAll("ul li").length)
+    console.log("Category: ", el.firstElementChild.textContent);
+    console.log("Elements: ", el.lastElementChild.children.length)
 });
 
