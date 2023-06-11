3d0fd009b45dfaa183be1ba2784f362c841906ca
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-06-11 14:05:38 +0300

    task-7 fixed

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const text = document.querySelector("#text");
 const rangeInput = document.querySelector("#font-size-control")
 
+text.style.fontSize = rangeInput.value + "px";
+
 rangeInput.addEventListener("input", (event) => {
-    text.style.fontSize = event.currentTarget.value + "px";
+    text.style.fontSize = rangeInput.value + "px";
 })
